f1de34055e29540fc0f22e9bacad82fb1ec4553d
Ryan Setzer <ryanjsetzer@gmail.com>
2022-02-21 10:52:36 -0500

    Initial Stub
    Beginning to stub out Inset class

---
+++ src/Interval.java
@@ -0,0 +1,8 @@
+/**
+ * Interval Class for PA 2.
+ * @author RYAN SETZER
+ * @version 1.0
+ */
+public class Interval {
+
+}
---
+++ src/Intset.java
@@ -0,0 +1,39 @@
+/**
+ * Intset Class for PA 2.
+ * @author RYAN SETZER
+ * @version 1.0
+ *
+ */
+public class Intset {
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public int size() {
+		return -1;
+	}
+	
+	public int getIntervalCount(Interval interval) {
+		return -1;
+	}
+	
+	public Interval getInterval(int i) {
+		return null;
+	}
+	
+	public boolean add(int i) {
+		return false;
+	}
+	
+	public void remove(int i) {
+	}
+	
+	public String toString() {
+		return null;
+	}
+	
+	public boolean equals(Object obj) {
+		return false;
+	}
+}
